d7a80a67c6c5b638739d9d84f8ef7958d3759949
Georgij Golaboski <georgij.golaboski@gmail.com>
2020-04-11 22:20:32 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -4,14 +4,31 @@ class Student {
 	String lastName;
 	int labPoints[];
 	//TODO constructor
-
+	Student() {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
 	//TODO seters & getters
+
 
 	public double getAverage() {
 		//TODO
+		int averagePoints = 0;
+		while(i<labPoints.length) {
+			averagePoints += labPoints[i];
+			i++;
+		}
+		return averagePoints / labPoints.length;
 	}
 
 	public boolean hasSignature() {
 		//TODO
-	}
+		if(getAverage() > 8) {
+			return true;
+		} else {
+			return false;
+		}
+ 	}
 }
